a0b39fe663d14132c1e98da561d5651cecc5c92a
kdubgotskillzb1890 <kwilkinson@westinghousehs.org>
2019-10-23 22:03:13 +0000

    added a click handeler, appended input to page

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+$("button").click(function()){
+var hello = $("input").val();
+$(".output").append(hello);
+hello = []
+
+
+}
 
 
 
-  
-  
-  
-  
+
+
+
 });
