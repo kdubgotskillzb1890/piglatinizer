fe768eeed6b7e2c87a8f922f0f6eda07a8904eab
kdubgotskillzb1890 <kwilkinson@westinghousehs.org>
2019-10-30 21:57:44 +0000

    work from 10-30-19

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
-$("button").click(function()){
+
+
+
+
+$("button").click(function() {
 var hello = $("input").val();
-$(".output").append(hello);
+$(".output").append (hello);
 hello = []
+hello.slice(0, 1)
+hello.slice(1,4)
 
 
-}
+
+})
 
 
 
